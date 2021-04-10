449ed5518f7661eba535873d5905224964a25635
imakarevich <ldaynekol@gmail.com>
2021-04-10 15:28:01 +0200

    lil fix

--- Lab1_Makarevich/Lab3/HttpTrigger1/index.js
+++ Lab1_Makarevich/Lab3/HttpTrigger1/index.js
@@ -1,11 +1,14 @@
 const PeopleDbContext = require('../DataAccess/db-context');
 const common = require('./../common');
 
-const FirstName = req.query.FirstName
-const LastName = req.query.LastName
+
 
 module.exports = async function (context, req) {
     await common.functionWrapper(context, req, async (body) => {
+
+        const FirstName = req.query.FirstName
+        const LastName = req.query.LastName
+
         const connectionString = process.env['PeopleDb'];
         const peopleDb = new PeopleDbContext(connectionString, context.log);
         
